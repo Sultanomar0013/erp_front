b9ef7f4c6aaba2ccbb4b5315affc119ae2ec8b95
Sultanomar0013 <sultanomar756@gmail.com>
2025-01-11 09:58:03 +0600

    table add

--- erp_front/vite.config.js
+++ erp_front/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    watch: {
+      usePolling: true, // Use polling instead of native file system watchers
+      interval: 100,    // Adjust the polling interval (default is 300 ms)
+      ignored: ['**/node_modules/**'], // Ignore large directories
+    },
+  },
+});
