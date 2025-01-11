54f01bd1269741952e2ca16ab9d36d41bd5f1244
Sultan Omar <90979782+Sultanomar0013@users.noreply.github.com>
2025-01-11 13:42:11 +0600

    Update testTable.jsx

--- erp_front/src/user_mod/components/member_basic/testTable.jsx
+++ erp_front/src/user_mod/components/member_basic/testTable.jsx
@@ -21,29 +21,28 @@ export default function ResponsiveDataGrid() {
   const rowCount = fakeData.length;
 
   return (
-    <Box sx={{ display:'flex', height: '100%',width:'100%',  flex:1 }}>
-        <Box sx={{ display:'flex',justifyContent:'center', height: 'auto',width:'100%',  flex:1 }}>
-            <Box sx={{ height: 400,width: '90%',}}>
-            <DataGrid
-              rows={rows}
-              columns={[
-                { field: 'id', headerName: 'ID', flex: 1 },
-                { field: 'userName', headerName: 'User Name', flex: 1 },
-                { field: 'email', headerName: 'Email', flex: 2 },
-                { field: 'email', headerName: 'Email', flex: 2 },
-              ]}
-              rowCount={rowCount}
-              paginationModel={paginationModel}
-              paginationMode="client"
-              onPaginationModelChange={setPaginationModel}
-              pageSizeOptions={[5, 10, 20]}
-              disableRowSelectionOnClick
-               components={{
-          Toolbar: QuickSearchToolbar,
-        }}
-            />
-            </Box>
+    <Box sx={{ display: 'flex', height: '100%', width: '100%', flex: 1 }}>
+      <Box sx={{ display: 'flex', justifyContent: 'center', height: 'auto', width: '100%', flex: 1 }}>
+        <Box sx={{ height: 400, width: '90%' }}>
+          <DataGrid
+            rows={rows}
+            columns={[
+              { field: 'id', headerName: 'ID', flex: 1 },
+              { field: 'userName', headerName: 'User Name', flex: 1 },
+              { field: 'email', headerName: 'Email', flex: 2 },
+            ]}
+            rowCount={rowCount}
+            paginationModel={paginationModel}
+            paginationMode="client"
+            onPaginationModelChange={setPaginationModel}
+            pageSizeOptions={[5, 10, 20]}
+            disableRowSelectionOnClick
+            components={{
+              Toolbar: QuickSearchToolbar,
+            }}
+          />
         </Box>
+      </Box>
     </Box>
   );
 }
